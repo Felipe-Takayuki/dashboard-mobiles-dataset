b0842d4240bb87e9c65bfc5eb5159b828f3c51e2
Felipe-Takayuki <felipe.takahashi@etec.sp.gov.br>
2025-05-05 19:08:14 -0300

    first commit

---
+++ dashboad/home.py
@@ -0,0 +1,5 @@
+import streamlit as st 
+
+def homePage(): 
+    st.set_page_config(layout="wide")
+    
---
+++ services/readcsv.py
@@ -0,0 +1,2 @@
+def readCSV():
+    df = pd.read_csv("../mobiles_dataset.csv", sep=";", decimal=",")
