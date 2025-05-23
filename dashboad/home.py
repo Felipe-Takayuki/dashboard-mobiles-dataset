2c8377f5ef0af8020586ffa7b457cc9abba04c81
mateussantospereira <matpereira0111@gmail.com>
2025-05-23 19:59:43 -0300

    Correção do CSV

--- dashboad/home.py
+++ dashboad/home.py
@@ -47,33 +47,51 @@ class HomePage:
         sns.barplot(x=avg_price.values, y=avg_price.index, palette="viridis", ax=ax1)
         st.pyplot(fig1)
 
-        # Gráfico 2: Preço vs Bateria
-        st.markdown("#### Preço vs Capacidade da Bateria")
+        # Gráfico 2: Marca vs Bateria
+        st.markdown("#### Média de Bateria (mAh) por Fabricante")
         fig2, ax2 = plt.subplots(figsize=(10, 6))
-        sns.scatterplot(data=df_filtered, x="Battery Capacity", y="Launched Price (USA)", hue="Company Name", ax=ax2)
+        avg_baterry = df_filtered.groupby("Company Name")["Battery Capacity"].mean().sort_values(ascending=False)
+        sns.barplot(x=avg_baterry.values, y=avg_baterry.index, palette="viridis", ax=ax2)
         st.pyplot(fig2)
 
         # Gráfico 3: Tamanho da Tela por Marca
-        st.markdown("#### Distribuição do Tamanho da Tela por Marca")
-        fig3, ax3 = plt.subplots(figsize=(12, 6))
-        sns.boxplot(data=df_filtered, x="Company Name", y="Screen Size", palette="Set3", ax=ax3)
-        ax3.tick_params(axis='x', rotation=45)
+        st.markdown("#### Média de Tamanho de Tela (inches) por Fabricante")
+        fig3, ax3 = plt.subplots(figsize=(10, 6))
+        avg_screen = df_filtered.groupby("Company Name")["Screen Size"].mean().sort_values(ascending=False)
+        sns.barplot(x=avg_screen.values, y=avg_screen.index, palette="viridis", ax=ax3)
         st.pyplot(fig3)
+#        st.markdown("#### Distribuição do Tamanho da Tela por Marca")
+#        fig3, ax3 = plt.subplots(figsize=(12, 6))
+#        sns.boxplot(data=df_filtered, x="Company Name", y="Screen Size", palette="Set3", ax=ax3)
+#        ax3.tick_params(axis='x', rotation=45)
+#        st.pyplot(fig3)
 
-        # Gráfico 4: Preço vs RAM
-        st.markdown("#### Preço vs Memória RAM")
+        # Gráfico 4: RAM por marca
+        st.markdown("#### Média de Memória RAM (GB) por Fabricante")
         fig4, ax4 = plt.subplots(figsize=(10, 6))
-        sns.scatterplot(data=df_filtered, x="RAM", y="Launched Price (USA)", hue="Company Name", ax=ax4)
+        avg_screen = df_filtered.groupby("Company Name")["RAM"].mean().sort_values(ascending=False)
+        sns.barplot(x=avg_screen.values, y=avg_screen.index, palette="viridis", ax=ax4)
         st.pyplot(fig4)
 
+#        st.markdown("#### Preço vs Memória RAM")
+#        fig4, ax4 = plt.subplots(figsize=(10, 6))
+#        sns.scatterplot(data=df_filtered, x="RAM", y="Launched Price (USA)", hue="Company Name", ax=ax4)
+#        st.pyplot(fig4)
+
         # Gráfico 5: Evolução do Preço Médio por Ano
-        st.markdown("#### Evolução do Preço Médio por Ano de Lançamento (EUA)")
-        fig5, ax5 = plt.subplots(figsize=(10, 6))
-        avg_by_year = df_filtered.groupby("Launched Year")["Launched Price (USA)"].mean()
-        sns.lineplot(x=avg_by_year.index, y=avg_by_year.values, marker="o", ax=ax5)
-        st.pyplot(fig5)
+        #st.markdown("#### Evolução do Preço Médio por Ano de Lançamento (EUA)")
+        #fig5, ax5 = plt.subplots(figsize=(10, 6))
+        #avg_by_year = df_filtered.groupby("Launched Year")["Launched Price (USA)"].mean()
+        #sns.lineplot(x=avg_by_year.index, y=avg_by_year.values, marker="o", ax=ax5)
+        #st.pyplot(fig5)
 
         # Gráfico 6: Preço vs Câmera Frontal
+        st.markdown("#### Média de MP da Câmera Frontal (MP) por Fabricante")
+        fig4, ax4 = plt.subplots(figsize=(10, 6))
+        avg_screen = df_filtered.groupby("Company Name")["Front Camera MP"].mean().sort_values(ascending=False)
+        sns.barplot(x=avg_screen.values, y=avg_screen.index, palette="viridis", ax=ax4)
+        st.pyplot(fig4)
+
         st.markdown("#### Preço vs Megapixels da Câmera Frontal")
         fig6, ax6 = plt.subplots(figsize=(10, 6))
         sns.scatterplot(data=df_filtered, x="Front Camera MP", y="Launched Price (USA)", hue="Company Name", ax=ax6)
